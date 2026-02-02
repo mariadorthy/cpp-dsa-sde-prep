01c23e1d3990cb58a86988bfe06923eac1706f62
Dorthy-d695 <dorthy695@gmail.com>
2026-02-02 07:37:34 +0530

    Added LeftTriangle.cpp

---
+++ patterns/basics/Left_aligned/LeftTriangle.cpp
@@ -0,0 +1,24 @@
+// Program to print a Left Triangle pattern
+// Input: n = 5
+// Output:
+// *
+// **
+// ***
+// ****
+// *****
+
+using namespace std;
+#include <iostream>
+#include <conio.h>
+
+int main(){
+    int n=5;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    getch();
+    return 0;
+}
